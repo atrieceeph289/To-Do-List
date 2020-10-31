a3a334e455edaae921f6b6ae40ccd900a30d3b39
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2020-10-31 10:19:13 +0000

    add edit button/function

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ groceryForm.addEventListener("submit", event => {
     if (groceryFormInput.value.length > 0) {
         // creation
         const listItem = document.createElement("li");
+        const editButton = document.createElement("span")
         const deleteButton = document.createElement("span");
         listItem.className = "list-item";
         deleteButton.className = "delete-button";
+        editButton.className = "edit-button"
 
         // manipulation
-
         listItem.innerText = groceryFormInput.value + " ";
         listItem.addEventListener("click", () => {
             listItem.classList.toggle("done");
@@ -21,9 +22,26 @@ groceryForm.addEventListener("submit", event => {
         deleteButton.addEventListener("click", () => {
             groceryList.removeChild(listItem);
         });
+        editButton.innerText = "edit"
+        editButton.addEventListener("click", () => {
+            let placeholder = listItem.innerText
+            let editVal = prompt("Edit todo", placeholder.substring(0, placeholder.length - 7))
+            if (editVal.length > 0) {
+                listItem.innerText = editVal;
+                listItem.appendChild(editButton)
+                listItem.appendChild(deleteButton);
+                listItem.classList.toggle("done");
+            } else {
+                listItem.classList.toggle("done");
+                return;
+            }
+
+
+        })
         groceryFormInput.value = ''
 
         // appending
+        listItem.appendChild(editButton)
         listItem.appendChild(deleteButton);
         groceryList.appendChild(listItem);
     }
